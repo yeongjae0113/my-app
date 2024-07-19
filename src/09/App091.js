d60188a3df8f10e5cac23f34de60cfd206f9c426
yeongjae0113 <dudwo0113@gmail.com>
2024-07-19 16:21:28 +0900

    reactPractice(3)_Commit 2024-07-19

--- src/09/App091.js
+++ src/09/App091.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { createRef, useRef, useState } from 'react';
+
+/**
+ * useRef()
+ *   https://react.dev/reference/react/useRef
+ *   주로, dom 을 변경할때 사용  ()
+ *   useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+ *
+ *   const ref = useRef(initialValue)
+ *      ↑↑↑ 참조를 return !
+ */
 
 const App091 = () => {
     console.log("App091 호출");
-    
+
+    const myRef = useRef(null);
+
+    const [list, setList] = useState([
+        {id: 1, name: '유인아'},
+        {id: 2, name: '베리베리'},
+    ]);
+
+    const myRefs = Array.from({length: list.length}).map(() => createRef());
+
     return (
         <div>
-            
+            <div ref={myRef}>박스</div>
+            <button onClick={() => {
+                console.log(myRef);
+                console.log(myRef.current);     // 참조하는 DOM 객체 !
+                myRef.current.style.backgroundColor = 'red';
+                myRefs[0].current.style.backgroundColor = 'limegreen';
+                myRefs[1].current.style.backgroundColor = 'lightblue';
+            }}>색 변경</button>
+            <hr/>
+            {list.map((user, index) => <div ref={myRefs[index]}>{user.id}:{user.name}</div>)}
         </div>
     );
 };
